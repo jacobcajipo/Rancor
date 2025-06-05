2264493cd2504bb70af1eeb252c9ac5a8f0973af
jacobcajipo <jacob.cajipo@gmail.com>
2025-06-05 19:34:02 +0800

    DSP2. add content. moved all scripts to deadspace.js for cleaner code

--- projects/work 1/Deadspace (2008)/js/deadspace.js
+++ projects/work 1/Deadspace (2008)/js/deadspace.js
@@ -12,5 +12,20 @@ let ambient_state = document.getElementById("ambient_button").innerHTML;
     document.getElementById("ambient").pause();
     document.getElementById("ambient_button").innerHTML = "PLAY";
   }
-
+}
+function open_lounge() {
+  document.getElementById("door_pad_lounge").style.display = "none";
+  document.getElementById("lounge").style.display = "flex";
+}
+function open_security() {
+  document.getElementById("door_pad_security").style.display = "none";
+  document.getElementById("security").style.display = "flex";
+}
+function open_quarantine() {
+  document.getElementById("console").style.display = "none";
+  document.getElementById("quarantine").style.display = "flex";
+  document.getElementById("next").style.display = "inline-block"; // Show the next button
+}
+function open_doorpads() {
+  document
 }
