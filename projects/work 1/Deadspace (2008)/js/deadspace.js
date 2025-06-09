bb208eb3d60f61d5e8efe91f42cbf09fc6f4da2d
jacobcajipo <jacob.cajipo@gmail.com>
2025-06-09 20:58:02 +0800

    Work 1. Sound script update.

--- projects/work 1/Deadspace (2008)/js/deadspace.js
+++ projects/work 1/Deadspace (2008)/js/deadspace.js
@@ -1,25 +1,36 @@
-document.getElementById("ambient_button").innerHTML = "PLAY";
+document.getElementById("ambient_button").innerHTML = "PAUSE";
 
 function ambient_music(){
 let ambient_state = document.getElementById("ambient_button").innerHTML;
+let lounge_is_open = document.getElementById("lounge").style.display == "flex";
    
   if (ambient_state == "PLAY"){
     document.getElementById("ambient").play();
-    //document.getElementById("ambient").playbackRate = 0.9;
     document.getElementById("ambient_button").innerHTML = "PAUSE";
+    if (lounge_is_open) {
+      document.getElementById("ambient_2").play();
+      document.getElementById("ambient_button").innerHTML = "PAUSE";
+    }
   }
   else {
     document.getElementById("ambient").pause();
+    document.getElementById("ambient_2").pause();
     document.getElementById("ambient_button").innerHTML = "PLAY";
   }
 }
 function open_lounge() {
   document.getElementById("door_pad_lounge").style.display = "none";
   document.getElementById("lounge").style.display = "flex";
+  document.getElementById("ambient_2").play();
 }
+
 function open_security() {
   document.getElementById("door_pad_security").style.display = "none";
   document.getElementById("security").style.display = "flex";
+
+    document.getElementById("ambient").pause();
+    document.getElementById("ambient_2").pause();
+    document.getElementById("heartbeat").play();
 }
 function open_quarantine() {
   document.getElementById("console").style.display = "none";
