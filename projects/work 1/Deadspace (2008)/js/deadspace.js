ab4a8f497d3ba7ce190d11379d1343a62eb12bfa
jacobcajipo <jacob.cajipo@gmail.com>
2025-06-24 19:53:45 +0800

    Deadspace CSS integration. UI overhaul. content update. sound update.

--- projects/work 1/Deadspace (2008)/js/deadspace.js
+++ projects/work 1/Deadspace (2008)/js/deadspace.js
@@ -1,23 +1,3 @@
-document.getElementById("ambient_button").innerHTML = "PAUSE";
-
-function ambient_music(){
-let ambient_state = document.getElementById("ambient_button").innerHTML;
-let lounge_is_open = document.getElementById("lounge").style.display == "flex";
-   
-  if (ambient_state == "PLAY"){
-    document.getElementById("ambient").play();
-    document.getElementById("ambient_button").innerHTML = "PAUSE";
-    if (lounge_is_open) {
-      document.getElementById("ambient_2").play();
-      document.getElementById("ambient_button").innerHTML = "PAUSE";
-    }
-  }
-  else {
-    document.getElementById("ambient").pause();
-    document.getElementById("ambient_2").pause();
-    document.getElementById("ambient_button").innerHTML = "PLAY";
-  }
-}
 function open_lounge() {
   document.getElementById("door_pad_lounge").style.display = "none";
   document.getElementById("lounge").style.display = "flex";
@@ -36,7 +16,4 @@ function open_quarantine() {
   document.getElementById("console").style.display = "none";
   document.getElementById("quarantine").style.display = "flex";
   document.getElementById("next").style.display = "inline-block"; // Show the next button
-}
-function open_doorpads() {
-  document
 }
