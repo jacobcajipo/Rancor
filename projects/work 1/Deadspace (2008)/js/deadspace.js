a4fd7b70694b64b2e600454babe8281fd9c96497
jc.desktop <jacob.cajipo@gmail.com>
2025-08-18 17:03:07 +0800

    footer optimized, part three content add. fixed audio glitches

--- projects/work 1/Deadspace (2008)/js/deadspace.js
+++ projects/work 1/Deadspace (2008)/js/deadspace.js
@@ -1,6 +1,7 @@
 onload = function() { 
   
 }
+//Part 2
 function open_lounge() {
   document.getElementById("door_pad_lounge").style.display = "none";
   document.getElementById("lounge").style.display = "flex";
@@ -16,8 +17,24 @@ function open_security() {
     document.getElementById("ambient_2").pause();
     document.getElementById("heartbeat").play();
 }
+//Part 3
 function open_quarantine() {
   document.getElementById("console").style.display = "none";
   document.getElementById("quarantine").style.display = "flex";
-
+}
+function pause_all_loops() {
+  document.getElementById('flatline_loop').pause();
+  document.getElementById('gun_1').pause();
+  document.getElementById('gun_2').pause();
+  document.getElementById('chase').pause();
+  document.getElementById('heartbeat').pause();
+  document.getElementById('slasher_1').pause();
+  document.getElementById('slasher_2').pause();    
+  document.getElementById('quarantine_normal').pause();
+  document.getElementById('quarantine_necro').pause();
+  document.getElementById('necro').pause();
+}
+function necro_scare_attach() {
+   document.getElementById('necro_elevator').style.position = 'fixed';
+   document.getElementById('necro_elevator').style.top = '50px';
 }
